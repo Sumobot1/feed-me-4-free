942a6d4c48c4b882d21277fa3622e4cd11e642aa
Michael Peet <ubernopeavi@gmail.com>
2016-09-16 23:52:09 -0400

    initial commit - empty project

---
+++ feedme4free/feedme4free/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for feedme4free project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feedme4free.settings")
+
+application = get_wsgi_application()
